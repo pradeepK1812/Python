7c1f270af7728331b84a345d6d836e3ae89a20f2
pradeepK1812 <pradeep.kalkhundiya@gmail.com>
2026-01-21 11:08:30 +0000

    problem2 committed

--- projects/dsa/src/dsa_examples/day5_sliding_window_prob1.py
+++ projects/dsa/src/dsa_examples/day5_sliding_window_prob1.py
@@ -1,3 +1,32 @@
+def get_first_neg_ksub(arr,k):
+    left = 0
+    right = 0
+    neg_list = []
+    window = []
+    # 1, -1, 2 -3
+    for right in range (len(arr)):
+        #do something
+        if(arr[right] <0):
+            #print("arr[right] is ", arr[right])
+            window.append(arr[right])
+        #add to list
+        if((right-left+1) == k):
+            #recored result in neg_list
+            print("window full case ", window)
+            if(len(window) == 0):
+                neg_list.append(0)
+            else:
+                neg_list.append(window[0])
+            
+            if(len(window) != 0) and  (arr[ left] == window[0]) :
+                print("going to pop", window[0])
+                window.pop(0)
+            left += 1
+            #increment the left
+    return neg_list
+
+
+
 #Given an array of integers and an integer k, find the maximum sum of any contiguous subarray of size k.
 def max_sum_subarray_k(arr, k):
     left = 0 
@@ -14,7 +43,14 @@ def max_sum_subarray_k(arr, k):
             left += 1
 
     return final_sum
+
+
 #calling main
 if __name__ == "__main__" :
     arr = [1,2,3,4,5]
-    print("final sum is ",max_sum_subarray_k(arr,3))
+    #print("final sum is ",max_sum_subarray_k(arr,3))
+    lst = [1,-1,2,-3,4,-5,6,-7]
+    print("Input lst is ", lst)
+    k =3 
+    print("subarry length is ",k)
+    print("list is ", get_first_neg_ksub(lst,k))
