c291be83326b39d4ca9a85cd9db77de353213a88
pradeepK1812 <pradeep.kalkhundiya@gmail.com>
2026-02-02 11:27:43 +0000

    day6 final problem

--- projects/dsa/src/dsa_examples/day6_two_pointer_prob.py
+++ projects/dsa/src/dsa_examples/day6_two_pointer_prob.py
@@ -1,3 +1,14 @@
+#Two Sum (sorted)
+
+#Reverse String
+
+#Remove Duplicates (sorted)
+
+#Container With Most Water
+
+#Trapping Rain Water 
+
+
 def two_sum_sorted(arr, s):
     left = 0
     right = len(arr)-1
@@ -21,6 +32,7 @@ def reverse_string(s):
         right-= 1
     return "".join(s1)
 
+
 def remove_duplicate_sorted(arr):
     i = 0
     print("len arr is" , len(arr))
@@ -30,11 +42,60 @@ def remove_duplicate_sorted(arr):
             arr[i] = arr[j]
     print(" i is arr is " , i,arr)
     return i+1
+
+#max_height container problem
+def max_height(height):
+    low = 0
+    n = len(height)
+    high = n-1
+    max_area = 0
+
+    while low<high:
+        width = high - low
+        length = min(height[low], height[high])
+        max_area = max(max_area, length*width)
+        if height[low] <height[high]:
+                     low += 1
+        else:
+                     high -= 1
+    return max_area    
+
+#  Given an array height where each bar has width 1,
+#compute how much water can be trapped after raining.
+def trap(arr):
+     left = 0
+     right = len(arr) -1 
+     max_left = 0
+     max_right = 0
+     water = 0
+     while left <right:
+         if arr[left] < arr[right]:
+             if arr[left] >= max_left:
+                 max_left =  arr[left]
+             else:
+                  water += max_left - arr[left]
+             left += 1
+         else:
+              if arr[right] >= max_right:
+                  max_right = arr[right]
+              else:
+                  water += max_right - arr[right]
+              right -= 1
+     return water
+
+
+
+##########################################main block##########################
 if __name__ == "__main__" :
     #arr = [1,2,3,4,5]
    # s = 10
    # print("return indices are" , two_sum_sorted(arr,s))
     #str1 = "howru"
    # print("reversed string is", reverse_string(str1))
-    arr = [1,1,2,2,3,3,4,5]
-    print("no of non duplicate elements in array is ", remove_duplicate_sorted(arr))
+    #arr = [1,1,2,2,3,3,4,5]
+    #print("no of non duplicate elements in array is ", remove_duplicate_sorted(arr))
+
+    #height = [1,8,6,2,5,4,8,3,7]
+    #print("max_area is ", max_height(height))
+    arr = [0,1,0,2,1,0,1,3,2,1,2,1]
+    print("max water trapped is ", trap(arr))
