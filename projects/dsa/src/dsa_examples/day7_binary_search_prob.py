b4a736f8ad12f6686ca4c22144cf5d08128d1384
pradeepK1812 <pradeep.kalkhundiya@gmail.com>
2026-02-09 14:32:19 +0000

    day 7 update

--- projects/dsa/src/dsa_examples/day7_binary_search_prob.py
+++ projects/dsa/src/dsa_examples/day7_binary_search_prob.py
@@ -35,10 +35,33 @@ def upper_bound(arr, num):
             upper = mid
     return lower
 
+########################################################################################
 
+
+def min_eating_speed(piles, h):
+    low = 0
+    high = max(piles)
+    while low < high:
+        mid = (low+high)//2
+        if can_finish(mid,piles,h):
+            high = mid
+        else:
+            low = mid+1
+    return low
+
+def can_finish(k,piles,H):
+    hours = 0
+    for pile in piles:
+        hours += (pile+k-1)//k
+    return hours <= H
+####################################################################
 
 
 if __name__ == "__main__":
-    arr = [3,4,2,2,7,8]
-    print("lower bound is ", lower_bound(arr, 2))
+    #arr = [3,4,2,2,7,8]
+    #print("lower bound is ", lower_bound(arr, 2))
    # print("then index of num is ", binary_search(arr,9))
+   H = 20
+   piles = [392, 493, 768,690,350]
+   print("min speed is " , min_eating_speed(piles,H))
+
