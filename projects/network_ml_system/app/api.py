ef3fb70c14607c9519b2e78c7d11ee20e3ff2a0d
pradeepK1812 <pradeep.kalkhundiya@gmail.com>
2026-05-07 11:29:30 +0000

    error checking added

--- projects/network_ml_system/app/api.py
+++ projects/network_ml_system/app/api.py
@@ -2,6 +2,11 @@ from fastapi import FastAPI
 from ml.model import NetworkModel
 import torch
 
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 app = FastAPI()
 
 # Load model once at startup
@@ -9,11 +14,31 @@ model = NetworkModel()
 model.load_state_dict(torch.load("ml/model.pt"))
 model.eval()
 
-@app.post("/predict")
-def predict(data: dict):
-    x_input = torch.tensor([data["features"]], dtype=torch.float32)
+#@app.post("/predict")
 
-    with torch.no_grad():
-        output = model(x_input)
+#from app.schemas import PredictionRequest
+from app.schemas import PredictionRequest, PredictionResponse
+from fastapi import HTTPException
 
-    return {"prediction": output.item()}
+@app.post("/predict", response_model=PredictionResponse)
+#@app.post("/predict")
+def predict(data: PredictionRequest):
+#def predict(data: dict):
+     try:
+        logger.info(f"Received input: {data.features}")
+
+        x_input = torch.tensor([data.features], dtype=torch.float32)
+
+        with torch.no_grad():
+            output = model(x_input)
+
+        return PredictionResponse(prediction=output.item())
+
+     except Exception as e:
+        logger.error(f"Prediction failed: {str(e)}")
+
+        raise HTTPException(
+            status_code=500,
+            detail="Internal server error"
+        )
+   
---
+++ projects/network_ml_system/app/schemas.py
@@ -0,0 +1,8 @@
+from pydantic import BaseModel
+from typing import List
+
+class PredictionRequest(BaseModel):
+    features: List[float]
+
+class PredictionResponse(BaseModel):
+    prediction: float
